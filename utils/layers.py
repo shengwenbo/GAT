0c3560e2801a568a110d2d039e4d5a784cc99117
shenwenbo <shwb159@qq.com>
2019-08-06 09:22:41 +0800

    1

--- utils/layers.py
+++ utils/layers.py
@@ -4,25 +4,33 @@ import tensorflow as tf
 conv1d = tf.layers.conv1d
 
 
-def attn_head(seq, out_sz, bias_mat, activation, in_drop=0.0, coef_drop=0.0, residual=False):
+def attn_head(seq, out_sz, bias_mat, activation, heads=3, in_drop=0.0, coef_drop=0.0, residual=False):
     with tf.name_scope('my_attn'):
         if in_drop != 0.0:
             seq = tf.nn.dropout(seq, 1.0 - in_drop)
 
-        seq_fts = tf.layers.conv1d(seq, out_sz, 1, use_bias=False)
+        batch_size = seq.shape[0]
+        n_nodes = seq.shape[1]
+
+        seq_fts = []
+        for _ in range(heads):
+            seq_fts.append(tf.layers.conv1d(seq, out_sz, 1, use_bias=False))
+        seq_fts = tf.concat(seq_fts, axis=1)
+        seq_fts = tf.reshape(seq_fts, [batch_size, n_nodes, heads, out_sz])
 
         # simplest self-attention possible
-        f_1 = tf.layers.conv1d(seq_fts, 1, 1)
-        f_2 = tf.layers.conv1d(seq_fts, 1, 1)
+        f_1 = tf.layers.conv1d(tf.reshape(seq_fts, [batch_size, n_nodes, -1]), 1, 1)
+        f_2 = tf.layers.conv1d(tf.reshape(seq_fts, [batch_size, n_nodes * heads, -1]), 1, 1)
         logits = f_1 + tf.transpose(f_2, [0, 2, 1])
-        coefs = tf.nn.softmax(tf.nn.leaky_relu(logits) + bias_mat)
+        coefs = tf.nn.softmax(tf.nn.leaky_relu(logits) + tf.tile(bias_mat, [1, 1, heads]))
 
         if coef_drop != 0.0:
-            coefs = tf.nn.dropout(coefs, 1.0 - coef_drop)
+            coefs_head = tf.nn.dropout(coefs, 1.0 - coef_drop)
         if in_drop != 0.0:
             seq_fts = tf.nn.dropout(seq_fts, 1.0 - in_drop)
 
-        vals = tf.matmul(coefs, seq_fts)
+
+        vals = tf.matmul(coefs_head, tf.reshape(seq_fts, [batch_size, -1, out_sz]))
         ret = tf.contrib.layers.bias_add(vals)
 
         # residual connection
